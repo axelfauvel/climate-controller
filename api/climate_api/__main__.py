4af3817a12fd340682ecd09b098c8fc0c76b97e6
Axel FAUVEL <afauvel.ext@orange.com>
2018-02-11 23:21:31 +0100

    refactoring repo and upgrading doc

--- api/climate_api/__main__.py
+++ api/climate_api/__main__.py
@@ -15,5 +15,6 @@ flask_api.add_resource(Temperature, '/v1/temperature')
 flask_api.add_resource(Timer, '/v1/timer')
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', port=8080, ssl_context='adhoc')
     ManageDatabase().init_db()
+    flask_app.run(host='0.0.0.0', port=8080, ssl_context='adhoc')
+

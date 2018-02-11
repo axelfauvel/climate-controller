a055a61350891a6f6f265a443d6989c7f6614fe9
Axel FAUVEL <afauvel.ext@orange.com>
2018-02-11 22:00:38 +0100

    typo

--- api/climate_api/__main__.py
+++ api/climate_api/__main__.py
@@ -12,7 +12,7 @@ flask_api = Api(flask_app)
 
 flask_api.add_resource(Power, '/v1/power/<status>')
 flask_api.add_resource(Temperature, '/v1/temperature')
-flask_api.add_resource(Timer, 'v1/timer')
+flask_api.add_resource(Timer, '/v1/timer')
 
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0', port=8080, ssl_context='adhoc')

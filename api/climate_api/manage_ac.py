52a24252e54e10dbbae5da5a9b640e8e95732f1a
Axel FAUVEL <afauvel.ext@orange.com>
2018-02-11 22:18:55 +0100

    changing timer poweroff behaviour

--- api/climate_api/manage_ac.py
+++ api/climate_api/manage_ac.py
@@ -21,8 +21,8 @@ def send_command(command):
 
 def timer(timer):
     time_to_wait = int(timer)*60
-    command = "sleep {} && irsend SEND_ONCE clim poweroff".format(
-        str(time_to_wait))
+    command = "sleep {} && curl -k https://127.0.0.1:8080/v1/power/poweroff".\
+        format(str(time_to_wait))
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True,
                      executable="/bin/bash")
     return True
